e9c4ed84065a34a9a2968355a9bbdaf85fa5adfb
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-23 22:14:45 +0300

    Update configuration files and fix linter errors

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 
 const App = () => {
-  return (
-   <BrowserRouter>
-     <Header>
-      <Routes>
-        <Route></Route>
-      </Routes>
-     </Header>
-   </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Header>
+                <Routes>
+                    <Route></Route>
+                </Routes>
+            </Header>
+        </BrowserRouter>
+    );
 };
 
 export default App;
