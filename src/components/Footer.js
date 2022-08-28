c56162b4bcd25c97ce695c0239a5ce86e8a077a6
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-29 00:18:47 +0300

    Add functionality to the nav links

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CSS/Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Camara Education Tanzania</p>
+            <p>Copyrigth © 2022 <a href="#">Camara Education,Ltd</a></p>
+        </div>
+    );
+};
+
+export default Footer;
