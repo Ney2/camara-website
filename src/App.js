db9fd34e386d5bb4ea27607739197eb75baa58cb
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-29 00:16:01 +0300

    Add path to the home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Header>
-                <Routes>
-                    <Route></Route>
-                </Routes>
-            </Header>
+            <Header />
+            <Routes>
+                <Route path="/"  element={<Home />} />
+            </Routes>
+            <Footer/>
         </BrowserRouter>
     );
 };
